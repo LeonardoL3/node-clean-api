76fdbfb2b016cb7c40ce21263f5ac68cbe430b04
Leonardo <lelazzati7@gmail.com>
2022-09-21 11:54:14 -0300

    refactor: i dont know

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -8,6 +8,7 @@ export class AccountMongoRepository implements AddAccountRepository {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection
       .insertOne(accountData)
+
     const account = { id: result.insertedId.toString(), ...accountData }
     return account
   }
