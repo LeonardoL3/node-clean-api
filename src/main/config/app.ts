84eea3db6db98b40800a48c3f4c1764622970121
Leonardo <lelazzati7@gmail.com>
2022-09-24 17:22:28 -0300

    feat: ensure Signup routes returns an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
+import setupRoutes from './routes'
 
 const app = express()
 setupMiddlewares(app)
+setupRoutes(app)
 
 app.get('/teste', (req: any, res: any) => {
   res.send(req.body)
